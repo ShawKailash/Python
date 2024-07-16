ff83f7e95ba67a44f555acdcd173c85904c232e7
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-07-16 19:47:27 +0530

    Add files via upload

---
+++ Python/P1_7_Dictionary.py
@@ -0,0 +1,11 @@
+person = {
+    "key": "value",
+    "name": "kailash",
+    "facebook": "kailash kumar"
+}
+print(person['facebook'])
+print(person)
+person['instagram']="shawkailash680"
+print(person)
+del person['key']
+print(person)
