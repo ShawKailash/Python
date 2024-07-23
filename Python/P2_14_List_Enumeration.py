e3e81fb902212cf0699a1d8cec1e8a23d44d8d15
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-07-23 16:07:07 +0530

    Add files via upload

---
+++ Python/P2_14_List_Enumeration.py
@@ -0,0 +1,7 @@
+animals = ["lion", "tiger", "elephant", "zebra", "horse"]
+for index, animal in enumerate(animals):
+    # if index%2==0:
+    #     continue
+    # print(index, animal)
+
+    print(f"{index+1}.\t{animal}")
